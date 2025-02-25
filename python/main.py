53fe6087b03a1287b9927f77cbe6997b6799f154
Sean Nguyen <30516148+seannguyn@users.noreply.github.com>
2025-02-25 17:31:00 +1100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -7,7 +7,7 @@ ENV = os.getenv("ENV", "local")
 
 @app.route('/', methods=['GET'])
 def home():
-    return return f"<h1>Hello, this is {ENV}</h1>"
+    return f"<h1>Hello, this is {ENV}</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
