8b0eab41343abb05f3f9da17d6fbbc14cb70d062
Sean Nguyen <30516148+seannguyn@users.noreply.github.com>
2025-02-25 14:29:51 +1100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+ENV = os.getenv("ENV", "local")
+
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>hello this is a test!</h1>"
+    return return f"<h1>Hello, this is {ENV}!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
